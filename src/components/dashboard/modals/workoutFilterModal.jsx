34dc52b834c22d769228d13ba60771ad5ae0040c
Sam Nunn <sam.nunn91@gmail.com>
2024-03-23 21:59:44 -0400

    Icons added foe Training Types

--- src/components/dashboard/modals/workoutFilterModal.jsx
+++ src/components/dashboard/modals/workoutFilterModal.jsx
@@ -11,6 +11,9 @@ import {
   ModalBody,
   ModalFooter,
 } from "@nextui-org/react";
+import BodybuildingIcon from "../../../assets/icons/bodybuilding-icon.png";
+import PowerbuildingIcon from "../../../assets/icons/powerbuilding-icon.png";
+import StrengthIcon from "../../../assets/icons/strength-icon.png";
 
 function WorkoutFilterModal({
   isModalOpen,
@@ -21,9 +24,16 @@ function WorkoutFilterModal({
   setWorkoutType,
   filteredWorkouts,
 }) {
+  const handleTrainingTypeChange = (e) => {
+    setTrainingType(e.target.value);
+  };
+  const handleWorkoutType = (e) => {
+    setWorkoutType(e.target.value);
+  };
   return (
     <Modal
       backdrop="opaque"
+      size="3xl"
       isOpen={isModalOpen}
       onClose={toggleModal}
       classNames={{
@@ -37,28 +47,70 @@ function WorkoutFilterModal({
               <h2 className="text-xl font-raleway pt-4">Choose your workout</h2>
             </ModalHeader>
             <ModalBody className="pt-2 pb-6">
-              <Select
-                label="Select a method of training"
-                className=""
-                value={trainingType}
-                onChange={(e) => setTrainingType(e.target.value)}>
-                {trainingTypeOptions.map((option) => (
-                  <SelectItem key={option.value} value={option.value}>
-                    {option.label}
-                  </SelectItem>
-                ))}
-              </Select>
-              <Select
-                label="Select a workout plan"
-                className=""
-                value={workoutType}
-                onChange={(e) => setWorkoutType(e.target.value)}>
-                {filteredWorkouts.map((option) => (
-                  <SelectItem key={option.value} value={option.value}>
-                    {option.label}
-                  </SelectItem>
-                ))}
-              </Select>
+              <div className="mb-4 w-full">
+                <div className="w-full flex flex-row justify-center gap-x-3">
+                  {trainingTypeOptions.map((option) => (
+                    <div key={option.value} className="">
+                      <label
+                        className={`flex flex-col items-center justify-center gap-y-1 cursor-pointer p-4 bg-gray-200 rounded-lg hover:bg-gray-300 w-[150px] h-[150px] ${
+                          trainingType === option.value
+                            ? "ring-2 ring-green-500"
+                            : ""
+                        }`}>
+                        <div
+                          style={{
+                            backgroundImage:
+                              option.value === "bodybuilding"
+                                ? `url(${BodybuildingIcon})`
+                                : option.value === "powerbuilding"
+                                ? `url(${PowerbuildingIcon})`
+                                : option.value === "strength"
+                                ? `url(${StrengthIcon})`
+                                : "",
+                          }}
+                          className="relative h-[65px] w-[250px] bg-cover bg-center bg-no-repeat"></div>
+                        <input
+                          type="radio"
+                          name="trainingType"
+                          value={option.value}
+                          checked={trainingType === option.value}
+                          onChange={handleTrainingTypeChange}
+                          className="mr-2 appearance-none" // Hide the default radio button appearance
+                        />
+                        <span className="select-none">{option.label}</span>
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <span className="text-lg font-semibold">
+                  Select a workout plan:
+                </span>
+                <div className="w-full flex flex-row justify-center gap-x-3">
+                  {filteredWorkouts.map((option) => (
+                    <div key={option.value} className="flex items-center m-2">
+                      <label
+                        className={`block cursor-pointer p-4 bg-gray-200 rounded-lg hover:bg-gray-300 w-[150px] h-[150px] ${
+                          workoutType === option.value
+                            ? "ring-2 ring-green-500"
+                            : ""
+                        }`}>
+                        <input
+                          type="radio"
+                          name="workoutType"
+                          value={option.value}
+                          checked={workoutType === option.value}
+                          onChange={handleWorkoutType}
+                          className="mr-2 appearance-none" // Hide the default radio button appearance
+                        />
+                        <span className="select-none">{option.label}</span>
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </ModalBody>
             <ModalFooter>
               <Button auto flat color="error" onPress={onClose}>
